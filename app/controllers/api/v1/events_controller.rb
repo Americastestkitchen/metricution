7191866344356feac53a970429d925faa6382265
Nathan Lilienthal <nathan@nixpulvis.com>
2014-05-14 21:59:32 -0400

    Don't send all data on event open.

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -5,12 +5,6 @@ module Api
 
       def bathrooms
         hijack do |websocket|
-          # Send all of the bathrooms when the client connects.
-          # This allows us to serve the client with one connection.
-          websocket.onopen do |data|
-            websocket.send_data(Metricution::ActiveRecordSerializer.to_json(Bathroom.all))
-          end
-
           # TODO: Use one Redis connection. Creating a thread with a full
           # Redis connection per request is pretty expensive.
           thread = Thread.new do
