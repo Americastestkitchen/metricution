659cbf6f9ccb5081d56a9c80280c11b76218a8ef
Nathan Lilienthal <nathan@nixpulvis.com>
2014-05-20 14:28:23 -0400

    Allow auto reloading code again.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,10 +4,13 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = true
+  config.eager_load = false
+
+  # We need a concurent server for SSEs.
+  config.allow_concurrency = true
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
