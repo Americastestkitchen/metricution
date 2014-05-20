aa8f64b29dc7f31376bc1d0d52e4297840a3e8b6
Nathan Lilienthal <nathan@nixpulvis.com>
2014-05-20 19:25:18 -0400

    Revert "Merge branch 'sse-instead-of-websockets'"
    This reverts commit 3981e80a039bf0a01dc90f9fbc3709c3ecfafdbd, reversing
    changes made to 6902e59ec43e753ae8f86ddae7225798153a13ec.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,9 +9,6 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # We need a concurent server for SSEs.
-  config.allow_concurrency = true
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
