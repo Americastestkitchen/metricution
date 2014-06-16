42fbe7cb2d2147596cf35b1ffdb66aee47cdda43
Nathan Lilienthal <nathan@nixpulvis.com>
2014-06-15 23:48:12 -0400

    Use SSEs across the board.

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,25 +1,32 @@
 module Api
   module V1
     class EventsController < BackendController
-      include Tubesock::Hijack
+      include ActionController::Live
+
+      before_action do
+        response.headers["Content-Type"] = "text/event-stream"
+      end
 
       def bathrooms
-        hijack do |websocket|
-          # TODO: Use one Redis connection. Creating a thread with a full
-          # Redis connection per request is pretty expensive.
-          thread = Thread.new do
-            Redis.new.subscribe('bathroom') do |on|
-              on.message do |channel, message|
-                websocket.send_data(message)
-              end
-            end
-          end
+        sse = Metricution::SSE::Writer.new(response.stream)
 
-          # Clean up the thread.
-          websocket.onclose do
-            thread.kill
+        # TODO: Use one Redis connection. Creating a full
+        # Redis connection per request is pretty expensive.
+        redis = Redis.new
+        redis.subscribe('bathroom') do |on|
+          on.message do |channel, message|
+            sse.write(message, event: 'bathroom')
           end
         end
+
+      # Rescue user closing connection.
+      rescue IOError
+        logger.into "stream closed"
+
+      # Clean up the action.
+      ensure
+        redis.quit
+        sse.close
       end
     end
   end
--- lib/metricution/sse.rb
+++ lib/metricution/sse.rb
@@ -76,7 +76,7 @@ module Metricution
 
       def write(object, options = {})
         options.each { |k,v| @io.write "#{k}: #{v}\n" }
-        @io.write "data: #{JSON.dump(object)}\n\n"
+        @io.write "data: #{object}\n\n"
       end
 
       def close
